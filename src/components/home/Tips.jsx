1b3d93bbd8ea508928241fb672963e648c4aeff3
Tommie <nguyentommie5@gmail.com>
2024-06-29 14:44:11 -0700

    Added title & body, change white to tan in tailwind

--- src/components/home/Tips.jsx
+++ src/components/home/Tips.jsx
@@ -1,5 +1,15 @@
 const Tips = () => {
-  return <div>Tips</div>;
+  return (
+    <div className="w-full flex justify-around bg-gardening-tan-100">
+      <div>img box</div>
+      <div className="flex flex-col w-1/5">
+        <p className="text-3xl font-bold mt-7">Some Gardening Tips</p>
+        <p className="my-7">
+          {"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium possimus facilis, maiores cum omnis obcaecati blanditiis voluptatum dignissimos accusamus, debitis excepturi consequatur soluta pariatur ducimus deserunt ex consectetur! Impedit, est!"}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Tips;
