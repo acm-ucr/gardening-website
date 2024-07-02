51ed32dd3ec897cca93af83ea9b73354f775c4fc
Howardwheeler <howard.wheeler159@gmail.com>
2024-07-02 15:41:23 -0700

    made values text on home screen

--- src/components/home/Values.jsx
+++ src/components/home/Values.jsx
@@ -1,5 +1,17 @@
 const Values = () => {
-  return <div>Our Values</div>;
+  return (
+    <div className="w-full flex flex-col justify-center bg-gardening-green-100">
+      <div className="text-3xl font-semibold text-center text-gardening-white-100 mt-5">
+        Our Values and Mission
+      </div>
+      <p className="m-0 p-0 text-center text-gardening-white-100 mb-5">
+        Our mission is Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+        enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        aliquip ex ea commodo consequat.
+      </p>
+    </div>
+  );
 };
 
 export default Values;
