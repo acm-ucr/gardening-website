38382263018958a44bc1762314e3b13a7f261ed9
전상하 <sangha@jeonsanghaui-MacBookPro.local>
2024-07-04 14:15:25 -0700

    Direction page made

--- src/components/home/Directions.jsx
+++ src/components/home/Directions.jsx
@@ -1,5 +1,16 @@
+import React from "react";
+
 const Directions = () => {
-  return <div>Directions</div>;
+  return (
+    <div className=" flex flex-col justify-center items-center bg-[#83694D] h-screen">
+      <div className="text-white font-extrabold text-6xl ">
+        How to get to Our Garden
+      </div>
+      <div className=" h-[70%] w-[90%] bg-[#C0BDA5] mt-[3%] mb-[3%] border-2 border-[#3E2913] rounded-lg">
+        {/* Div for rectangular empty div space*/}
+      </div>
+    </div>
+  );
 };
 
 export default Directions;
