a5188cdc21d49b86156e870e74934c94855219b9
fardinzam <fardinzaman51@gmail.com>
2024-07-08 15:15:10 -0700

    small changes to proportions

--- src/components/home/Tips.jsx
+++ src/components/home/Tips.jsx
@@ -1,18 +1,16 @@
 const Tips = () => {
   return (
-    <div className="min-h-screen flex items-center justify-around bg-gardening-tan-100 p-10">
-      <div className="flex items-center space-x-10">
-        <div className="w-96 h-96 bg-gardening-tan-200 border-2 border-black rounded-lg flex items-center justify-center"></div>
-        <div className="flex flex-col w-96">
-          <p className="text-5xl font-bold text-center mb-4">
-            Some Gardening Tips
-          </p>
-          <p className="text-lg text-center">
-            {
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-            }
-          </p>
-        </div>
+    <div className="p-10 flex items-center justify-around bg-gardening-tan-100">
+      <div className="w-96 h-96 bg-gardening-tan-200 border-[3px] border-gardening-brown-100 rounded-lg" />
+      <div className="flex flex-col w-2/5">
+        <p className="text-5xl font-bold text-center mb-4">
+          Some Gardening Tips
+        </p>
+        <p className="text-2xl font-semibold text-center">
+          {
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          }
+        </p>
       </div>
     </div>
   );
--- src/components/home/WhyJoin.jsx
+++ src/components/home/WhyJoin.jsx
@@ -2,12 +2,12 @@ const WhyJoin = () => {
   // return <div>Join</div>;
   return (
     <div className="w-full flex flex-col items-center bg-gardening-yellow-100 border-y-8 border-gardening-yellow-200">
-      <p className="text-5xl font-bold text-center text-gardening-white-100 my-12">
+      <p className="text-5xl font-bold text-center text-gardening-tan-100 my-12">
         Why Join?
       </p>
 
       <div className="w-5/6 flex justify-between mb-10">
-        <div className="w-1/6 flex flex-col items-center justify-center rounded-full border-gardening-yellow-200 border-4 bg-gardening-white-100 py-2">
+        <div className="w-1/6 flex flex-col items-center justify-center rounded-full border-gardening-yellow-200 border-4 bg-gardening-tan-100 py-4">
           <p className="text-5xl font-bold text-gardening-yellow-200">100+</p>
 
           <p className="text-2xl font-medium text-gardening-yellow-200">
@@ -15,7 +15,7 @@ const WhyJoin = () => {
           </p>
         </div>
 
-        <div className="w-1/6 flex flex-col items-center justify-center rounded-full border-gardening-yellow-200 border-4 bg-gardening-white-100 py-2">
+        <div className="w-1/6 flex flex-col items-center justify-center rounded-full border-gardening-yellow-200 border-4 bg-gardening-tan-100 py-4">
           <p className="text-5xl font-bold text-gardening-yellow-200">200+</p>
 
           <p className="text-2xl font-medium text-gardening-yellow-200">
@@ -23,7 +23,7 @@ const WhyJoin = () => {
           </p>
         </div>
 
-        <div className="w-1/6 flex flex-col items-center justify-center rounded-full border-gardening-yellow-200 border-4 bg-gardening-white-100 py-2">
+        <div className="w-1/6 flex flex-col items-center justify-center rounded-full border-gardening-yellow-200 border-4 bg-gardening-tan-100 py-4">
           <p className="text-5xl font-bold text-gardening-yellow-200">50+</p>
 
           <p className="text-2xl font-medium text-gardening-yellow-200">
