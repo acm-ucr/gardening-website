8a7c147e94ad862160b9b8f51d05ffd6f0bdb664
전상하 <sangha@jeonsancBookPro.lan>
2024-07-08 16:35:43 -0700

    fixed feedbacks

--- src/components/home/Directions.jsx
+++ src/components/home/Directions.jsx
@@ -2,13 +2,11 @@ import React from "react";
 
 const Directions = () => {
   return (
-    <div className=" flex flex-col justify-center items-center bg-[#83694D] h-screen">
+    <div className=" flex flex-col justify-center items-center bg-gardening-brown-200 h-screen">
       <div className="text-white font-extrabold text-6xl ">
         How to get to Our Garden
       </div>
-      <div className=" h-[70%] w-[90%] bg-[#C0BDA5] mt-[3%] mb-[3%] border-2 border-[#3E2913] rounded-lg">
-        {/* Div for rectangular empty div space*/}
-      </div>
+      <div className=" h-[70%] w-[90%] bg-gardening-yellow-300 my-[2%] border-2 border-gardening-brown-300 rounded-lg"></div>
     </div>
   );
 };
