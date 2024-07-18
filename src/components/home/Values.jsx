4fc81b851d38da28cab6c50436976b544c2f210d
Tommie <nguyentommie5@gmail.com>
2024-07-18 13:23:06 -0700

    Added section cards and labels

--- src/components/home/Values.jsx
+++ src/components/home/Values.jsx
@@ -1,10 +1,35 @@
 const Values = () => {
   return (
-    <div className="w-full flex flex-col items-center bg-gardening-green-100">
-      <p className="text-3xl font-semibold text-gardening-white-100 mt-5">
+    <div className="w-full flex flex-col items-center bg-gardening-green-100 py-10">
+      <p className="text-5xl font-bold text-gardening-white-100 mt-5 mb-10">
         Our Values and Mission
       </p>
-      <p className="w-3/5 m-0 p-0 text-center text-gardening-white-100 mb-5">
+
+      <div className="flex justify-around w-full max-w-4xl mb-10">
+        <div className="flex flex-col items-center mx-4 mb-6">
+          <div className="bg-gardening-white-100 w-30 h-35 p-4 rounded shadow-md transform rotate-2 mb-4 flex flex-col items-center">
+            <div className="w-16 h-16 bg-red-200 rounded mb-2"></div>
+            <p className="text-xl font-semibold text-gardening-brown-200">Seeds</p>
+          </div>
+          <p className="text-4xl font-bold text-gardening-white-100 mt-2">Value</p>
+        </div>
+        <div className="flex flex-col items-center mx-4 mb-6">
+          <div className="bg-gardening-white-100 w-30 h-35 p-4 rounded shadow-md transform -rotate-2 mb-4 flex flex-col items-center">
+            <div className="w-16 h-16 bg-yellow-100 rounded mb-2"></div>
+            <p className="text-xl font-semibold text-gardening-brown-200">Seeds</p>
+          </div>
+          <p className="text-4xl font-bold text-gardening-white-100 mt-2">Value</p>
+        </div>
+        <div className="flex flex-col items-center mx-4 mb-6">
+          <div className="bg-gardening-white-100 w-30 h-35 p-4 rounded shadow-md transform rotate-1 mb-4 flex flex-col items-center">
+            <div className="w-16 h-16 bg-yellow-500 rounded mb-2"></div>
+            <p className="text-xl font-semibold text-gardening-brown-200">Seeds</p>
+          </div>
+          <p className="text-4xl font-bold text-gardening-white-100 mt-2">Value</p>
+        </div>
+      </div>
+
+      <p className="w-3/5 text-2xl text-center text-gardening-white-100">
         Our mission is Lorem ipsum dolor sit amet, consectetur adipiscing elit,
         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
         enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
