76d3f213bc0bae357c90f6418cd68276e58df946
fardinzam <fardinzaman51@gmail.com>
2024-07-21 01:15:22 -0700

    some small edits

--- src/components/home/Join.jsx
+++ src/components/home/Join.jsx
@@ -3,27 +3,24 @@ import Link from "next/link";
 
 const Join = () => {
   return (
-    <div className="p-10 flex-col items-center justify-around bg-gardening-tan-1oo">
-      <div className="flex justify-center">
-        <div className="w-2/5">
-          <p className="text-5xl font-bold text-left mb-4 ">
-            Join UCR Gardening!
+    <div className="flex-col">
+      <div className="flex justify-around py-10">
+        <div className="w-2/5 flex flex-col">
+          <p className="my-3 text-5xl font-bold">Join UCR Gardening!</p>
+          <p className="my-2 text-2xl font-semibold">
+            {
+              "Get involved by Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            }
           </p>
-          <p className="text-2xl font-semibold text-left mr-10 mb-10">
-            Get involved by Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-            laboris nisi ut aliquip ex ea commodo consequat.
-          </p>
-          <div className="flex">
-            <Link
-              href="https://discord.com/invite/T2xVxxMtcx"
-              className="border-2 border-black rounded-full pb-2.5 pt-5 w-2/5 text-4xl font-bold text-center justify-center mx-auto mb-10 "
-            >
-              JOIN NOW!
-            </Link>
-          </div>
+          <Link
+            href="https://discord.com/invite/T2xVxxMtcx"
+            className="w-[50%] mt-5 pt-3 text-4xl font-bold text-center self-center border-2 border-black rounded-full"
+          >
+            JOIN NOW!
+          </Link>
         </div>
+
+        <div className="w-[30%]">img here</div>
       </div>
 
       <WhyJoin />
