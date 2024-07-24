954797a4316fb7b6edf861f630dced03e0b087c6
전상하 <sangha@jeonsanghaui-MacBookPro.local>
2024-07-25 01:58:25 +0900

    Made data file for better access of Board Grid

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -1,10 +1,27 @@
-import GroupPic from "@/components/board/GroupPic";
+"use client";
+import { BoardData } from "@/data/board.js";
+import Card from "@/components/board/Card";
 
 const Board = () => {
   return (
-    <div>
-      Board
-      <GroupPic />
+    <div className="w-5/6 mx-auto">
+      <div className="text-white text-6xl flex flex-col justify-center items-center my-[4%]">
+        Meet the Team!
+      </div>
+      <div className="flex justify-center">
+        <div className="grid grid-cols-4 grid-flow-row gap-x-36 gap-y-10">
+          {BoardData.map((CARD, index) => (
+            <Card
+              Name={CARD.Name}
+              Position={CARD.Position}
+              image={CARD.image}
+              Year={CARD.Year}
+              key={index}
+              custom={index * 0.1}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/data/board.js
@@ -0,0 +1,52 @@
+import cat from "@/public/catOrangeGardening.webp";
+
+export const BoardData = [
+  {
+    Name: "1 name",
+    Position: "1 position",
+    Year: "1 year",
+    image: cat,
+  },
+  {
+    Name: "2 name",
+    Position: "2 position",
+    Year: "2 year",
+    image: cat,
+  },
+  {
+    Name: "3 name",
+    Position: "3 position",
+    Year: "3 year",
+    image: cat,
+  },
+  {
+    Name: "4 name",
+    Position: "4 position",
+    Year: "4 year",
+    image: cat,
+  },
+  {
+    Name: "5 name",
+    Position: "5 position",
+    Year: " 5year",
+    image: cat,
+  },
+  {
+    Name: "6 name",
+    Position: "6 position",
+    Year: "6 year",
+    image: cat,
+  },
+  {
+    Name: "7 name",
+    Position: "7 position",
+    Year: "7 year",
+    image: cat,
+  },
+  {
+    Name: "8 name",
+    Position: "8 position",
+    Year: "8 year",
+    image: cat,
+  },
+];
