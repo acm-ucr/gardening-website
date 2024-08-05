eb6dc273a996a7dca0f83787a870c0f9d57021f2
Howardwheeler <howard.wheeler159@gmail.com>
2024-08-04 23:22:36 -0700

    fixed Upcoming events page

--- src/components/events/Upcoming.jsx
+++ src/components/events/Upcoming.jsx
@@ -1,51 +1,51 @@
 const Upcoming = () => {
   return (
     <div className="">
-      <div className="flex flex-col m-[3%]">
-        <div className="font-bold sm:text-6xl md:text-7xl lg:text-8xl ml-[3%] mt-[3%] text-gardening-brown-100">
+      <div className="flex flex-col m-[2%]">
+        <div className="font-bold sm:text-5xl md:text-6xl lg:text-7xl ml-[2%] mt-[2%] text-gardening-brown-100">
           Upcoming Events
         </div>
         <div className="w-full flex sm:flex-col md:flex-col lg:flex-row justify-center items-center items-stretch">
-          <div className="flex flex-row m-[3%] rounded-lg drop-shadow-lg overflow-hidden">
-            <div className="w-1/3 flex flex-col justify-center items-center font-bold sm:text-3xl md:text-5xl lg:text-4xl xl:text-5xl bg-gardening-white-100">
-              <p className="mt-[5%]">JUN</p>
+          <div className="flex flex-row mx-[2%] mt-[1%] rounded-lg drop-shadow-lg overflow-hidden">
+            <div className="w-1/3 flex flex-col justify-center items-center sm:text-2xl md:text-4xl lg:text-3xl xl:text-4xl font-bold bg-gardening-white-100">
+              <p className="">JUN</p>
               <p className="sm:text-8xl md:text-9xl lg:text-8xl xl:text-9xl">
                 23
               </p>
-              <p className="-mt-[5%]">6-8 PM</p>
+              <p className="-my-[10%]">6-8 PM</p>
             </div>
             <div className="w-2/3 font-bold text-gardening-white-100 bg-gardening-yellow-200">
-              <div className="m-[7%]">
+              <div className="m-[5%]">
                 <p className="sm:text-3xl md:text-4xl lg:text-3xl xl:text-4xl">
                   Event Name
                 </p>
-                <p className="sm:text-xl md:text-2xl lg:text-xl xl:text-2xl my-[2%]">
+                <p className="sm:text-xl md:text-2xl lg:text-xl xl:text-2xl">
                   Location
                 </p>
-                <p className="sm:text-md md:text-lg lg:text-sm xl:text-lg font-semibold">
+                <p className="sm:text-md md:text-lg lg:text-sm xl:text-lg font-normal">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </p>
               </div>
             </div>
           </div>
-          <div className="flex flex-row m-[3%] rounded-lg drop-shadow-lg overflow-hidden">
-            <div className="w-1/3 flex flex-col justify-center items-center font-bold sm:text-3xl md:text-5xl lg:text-4xl xl:text-5xl bg-gardening-white-100">
-              <p className="mt-[5%]">JUN</p>
+          <div className="flex flex-row mx-[2%] mt-[1%] rounded-lg drop-shadow-lg overflow-hidden">
+            <div className="w-1/3 flex flex-col justify-center items-center sm:text-2xl md:text-4xl lg:text-3xl xl:text-4xl font-bold bg-gardening-white-100">
+              <p className="">JUN</p>
               <p className="sm:text-8xl md:text-9xl lg:text-8xl xl:text-9xl">
                 24
               </p>
-              <p className="-mt-[5%]">7 PM</p>
+              <p className="-my-[10%]">7 PM</p>
             </div>
             <div className="w-2/3 font-bold text-gardening-white-100 bg-gardening-yellow-200">
-              <div className="m-[7%]">
+              <div className="m-[5%]">
                 <p className="sm:text-3xl md:text-4xl lg:text-3xl xl:text-4xl">
                   Event Name
                 </p>
-                <p className="sm:text-xl md:text-2xl lg:text-xl xl:text-2xl my-[2%]">
+                <p className="sm:text-xl md:text-2xl lg:text-xl xl:text-2xl">
                   Location
                 </p>
-                <p className="sm:text-md md:text-lg lg:text-sm xl:text-lg font-semibold">
+                <p className="sm:text-md md:text-lg lg:text-sm xl:text-lg font-normal">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </p>
@@ -54,46 +54,46 @@ const Upcoming = () => {
           </div>
         </div>
         <div className="flex sm:flex-col md:flex-col lg:flex-row items-center items-stretch">
-          <div className="flex flex-row m-[3%] rounded-lg drop-shadow-lg overflow-hidden">
-            <div className="w-1/3 flex flex-col justify-center items-center font-bold sm:text-3xl md:text-5xl lg:text-4xl xl:text-5xl bg-gardening-white-100">
-              <p className="mt-[5%]">JUN</p>
+          <div className="flex flex-row mx-[2%] mt-[1%] rounded-lg drop-shadow-lg overflow-hidden">
+            <div className="w-1/3 flex flex-col justify-center items-center sm:text-2xl md:text-4xl lg:text-3xl xl:text-4xl font-bold bg-gardening-white-100">
+              <p className="">JUN</p>
               <p className="sm:text-8xl md:text-9xl lg:text-8xl xl:text-9xl">
                 25
               </p>
-              <p className="-mt-[5%]">7 PM</p>
+              <p className="-my-[10%]">7 PM</p>
             </div>
             <div className="w-2/3 font-bold text-gardening-white-100 bg-gardening-yellow-200">
-              <div className="m-[7%]">
+              <div className="m-[5%]">
                 <p className="sm:text-3xl md:text-4xl lg:text-3xl xl:text-4xl">
                   Event Name
                 </p>
-                <p className="sm:text-xl md:text-2xl lg:text-xl xl:text-2xl my-[2%]">
+                <p className="sm:text-xl md:text-2xl lg:text-xl xl:text-2xl">
                   Location
                 </p>
-                <p className="sm:text-md md:text-lg lg:text-sm xl:text-lg font-semibold">
+                <p className="sm:text-md md:text-lg lg:text-sm xl:text-lg font-normal">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </p>
               </div>
             </div>
           </div>
-          <div className="flex flex-row m-[3%] rounded-lg drop-shadow-lg overflow-hidden">
-            <div className="w-1/3 flex flex-col justify-center items-center font-bold sm:text-3xl md:text-5xl lg:text-4xl xl:text-5xl bg-gardening-white-100">
-              <p className="mt-[5%]">JUN</p>
+          <div className="flex flex-row mx-[2%] mt-[1%] rounded-lg drop-shadow-lg overflow-hidden">
+            <div className="w-1/3 flex flex-col justify-center items-center sm:text-2xl md:text-4xl lg:text-3xl xl:text-4xl font-bold bg-gardening-white-100">
+              <p className="">JUN</p>
               <p className="sm:text-8xl md:text-9xl lg:text-8xl xl:text-9xl">
                 26
               </p>
-              <p className="-mt-[5%]">7 PM</p>
+              <p className="-my-[10%]">7 PM</p>
             </div>
             <div className="w-2/3 font-bold text-gardening-white-100 bg-gardening-yellow-200">
-              <div className="m-[7%]">
+              <div className="m-[5%]">
                 <p className="sm:text-3xl md:text-4xl lg:text-3xl xl:text-4xl">
                   Event Name
                 </p>
-                <p className="sm:text-xl md:text-2xl lg:text-xl xl:text-2xl my-[2%]">
+                <p className="sm:text-xl md:text-2xl lg:text-xl xl:text-2xl">
                   Location
                 </p>
-                <p className="sm:text-md md:text-lg lg:text-sm xl:text-lg font-semibold">
+                <p className="sm:text-md md:text-lg lg:text-sm xl:text-lg font-normal">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </p>
