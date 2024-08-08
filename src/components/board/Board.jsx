5112ac7751e5d6aa72ce193ab1a99e1d4c573701
전상하 <sangha@jeonsanghaui-MacBookPro.local>
2024-08-09 01:55:24 +0900

    Fixed responsiveness, cards, bugs

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -6,10 +6,10 @@ import Title from "@/components/board/Title";
 
 const Board = () => {
   return (
-    <div className="w-5/6 mx-auto">
+    <div className="w-5/6 mx-auto relative z-0">
       <Title />
-      <div className="flex justify-center">
-        <div className="grid grid-cols-4 grid-flow-row gap-x-36 gap-y-10">
+      <div className="flex justify-center relative z-10 pt-40">
+        <div className="grid grid-cols-2  md:grid-cols-4 grid-flow-row gap-x-10 gap-y-20 md:gap-x-16 lg:gap-x-32 lg:gap-y-24  -mt-8 md:mt-0  lg:mt-20 mb-10">
           {BoardData.map((CARD, index) => (
             <Card
               Name={CARD.Name}
