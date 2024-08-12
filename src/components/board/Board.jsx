02d8ff41e41553341dc477192d4264c48b0cf2f4
fardinzam <fardinzaman51@gmail.com>
2024-08-12 01:44:52 -0700

    removed red semicircle

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -2,14 +2,13 @@
 import { BoardData } from "@/data/board.js";
 import Card from "@/components/board/Card";
 import GroupPic from "@/components/board/GroupPic";
-import Title from "@/components/board/Title";
 
 const Board = () => {
   return (
     <div className="w-5/6 mx-auto relative z-0">
-      <Title />
-      <div className="flex justify-center relative z-10 pt-40">
-        <div className="grid grid-cols-2  md:grid-cols-4 grid-flow-row gap-x-10 gap-y-20 md:gap-x-16 lg:gap-x-32 lg:gap-y-24  -mt-8 md:mt-0  lg:mt-20 mb-10">
+      <p className="my-10 text-7xl font-bold text-gardening-brown-100">Board</p>
+      <div className="flex justify-center">
+        <div className="grid grid-cols-2  md:grid-cols-4 grid-flow-row gap-x-10 gap-y-20 md:gap-x-16 lg:gap-x-32 lg:gap-y-24 mb-10">
           {BoardData.map((CARD, index) => (
             <Card
               Name={CARD.Name}
