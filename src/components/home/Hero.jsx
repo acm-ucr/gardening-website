d28cf2819cad63479a48bf7bf6de24e8363dd1b7
John Zhou <zhou9865@gmail.com>
2024-09-02 21:38:42 -0700

    circle problems

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -11,9 +11,12 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="w-2/5 h-[30vw] border-[2px] rounded-lg border-gardening-brown-100 bg-gardening-tan-200"></div>
+      <div className="w-2/5 h-[30vw] border-[2px] rounded-lg border-gardening-brown-100 bg-gardening-tan-200 opacity-50"></div>
 
-      <div className="w-2/5 h-[100%] -mt-20 bg-gardening-red-100 border-gardening-red-200 border-l-[20px] border-y-[20px] rounded-l-full absolute right-0 z-[-1]" />
+      {/* <div className="w-2/5 h-[100%] -mt-20 bg-gardening-red-100 border-gardening-red-200 border-l-[20px] border-y-[20px] rounded-l-full absolute right-0 z-[-1]" /> */}
+      <div className="flex flex-col justify-center">
+        <div className="overflow-visible w-[11rem] h-[110%] sm:w-[30%] sm:h-[110%] lg:w-[30%] lg:h-[110%] bg-gardening-red-100 border-gardening-red-200 border-l-[20px] border-y-[20px] lg:border-l-[40px] lg:border-y-[25px] rounded-l-full absolute right-0 z-[-1]" />
+      </div>
     </div>
   );
 };
